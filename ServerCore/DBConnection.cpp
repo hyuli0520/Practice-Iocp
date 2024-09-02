898d6281588f8425e13d1d2af936aaa9b54a9d4f
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2024-09-02 23:57:49 +0900

    Database-> getTable & insert & select

--- ServerCore/DBConnection.cpp
+++ ServerCore/DBConnection.cpp
@@ -12,9 +12,22 @@ bool DBConnection::Connect()
 		const char* url = ("mysqlx://root:1234@127.0.0.1");
 		cout << "Creating session on" << url << "..." << endl;
 		mysqlx::Session session(url);
-		cout << "Session accepted, creating collections..." << endl;
 
+		cout << "Session accepted, getting collections..." << endl;
 		mysqlx::Schema schema = session.getSchema("sunnight");
+		mysqlx::Table table = schema.getTable("player");
+
+
+		//{
+		//	auto insert = table.insert("player_id", "player_name", "player_location")
+		//		.values(2, "admin2", "10010").execute();
+		//}
+		
+		auto result = table.select("player_id", "player_name", "player_location").execute();
+
+		for (mysqlx::Row row : result)
+			cout << "player_id: " << row[0] << ", player_name: " << row[1] << ", player_location: " << row[2] << endl;
+
 	}
 	catch (const mysqlx::Error& err)
 	{
